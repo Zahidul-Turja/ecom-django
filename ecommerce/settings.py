de9173e7abca32c07ca0759f25ed624da23568b5
Zahidul-Turja <zahidulturja@gmail.com>
2023-11-22 13:02:45 +0600

    Store setup

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -1,4 +1,5 @@
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -92,12 +93,11 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = "static/"
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
